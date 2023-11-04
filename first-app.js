14a2b49dc6f2fe5c83438ad156536493d9582efc
Sreejeeshkumar <sreeapp1947@gmail.com>
2023-11-04 16:14:38 +0530

    2nd task

--- first-app.js
+++ first-app.js
@@ -1,3 +1,15 @@
-const fs=require('fs')
-
-fs.writeFileSync('hello.text', 'hello world')
+const arrowFunction=(a,b)=>{
+    return a*b
+}
+console.log(arrowFunction(3,2))
+const student={
+    name:'Max',
+    age:16,
+    Marks:80,
+    greet() {
+        console.log('I am '+this.name+' and I am '+this.age+' year old with '+this.Marks+' marks')
+    }
+    
+    
+}
+student.greet();
