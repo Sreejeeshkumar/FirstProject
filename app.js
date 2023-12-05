5a0ea255e2299b1204b5c22bf58357a0c61261fc
Sreejeeshkumar <sreeapp1947@gmail.com>
2023-12-05 09:57:59 +0530

    routes

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-const http = require('http');
-const fs=require('fs')
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
-    const url=req.url
-    const method=req.method
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-       return res.end();
-        console.log(req.url)
-    }
-    if(url === '/message' && method==='POST')
-    {
-        const body=[]
-        req.on('data',(chunk)=>{
-            console.log(chunk)
-            body.push(chunk)         
-        })
-        req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString()
-            const message=parsedBody.split('=')[1]   
-            console.log(message)
-            fs.writeFileSync('message.txt', message)   
- 
-         })
- 
-         res.statusCode=302
-            res.setHeader('Location','/')
-            return res.end()
-        }
-    });
+ const http = require('http');
 
-    server.listen(4000);
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(4000);
+
+
